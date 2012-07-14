5b8a0c01da320c13c678dd6b92f9eac5b33b06a7
Brian LeRoux <brian@westcoastlogic.com>
2012-07-14 14:40:14 -0700

    create working w/ new structure

--- cordova.js
+++ cordova.js
@@ -1,64 +1,11 @@
-var fs = require('fs'),
-    util = require('util'),
-    spawn = require('child_process').spawn;
+var fs        = require('fs')
+,   path      = require('path')
+,   util      = require('util')
+,   exec      = require('child_process').exec
+,   platforms = ['ios', 'android']
+,   dist      = process.env.CORDOVA_HOME != undefined ? process.env.CORDOVA_HOME : path.join(__dirname, 'lib', 'cordova-1.9.0')
 
-var platforms = ['ios', 'android'];
-var actions = ['create', 'build', 'emulate'];
-var action = 'create';
-
-// TODO: implement reading/writing cordova.conf
-
-function usage(code) {
-    console.error("usage:  cordova create [platform:[directory]:[package_name]:[project_name]]...|[cordova.conf]");
-    console.error("\tcordova build directory directory...|cordova.conf");
-    console.error("\tcordova emulate directory directory...|cordova.conf");
-    process.exit(code);
-}
-
-function run(action, platform, directory, options) {
-   switch(action) {
-        case 'create':
-            // if used with the cordova distribution
-            if(!fs.exists(directory + '/lib/' + platform)) {
-                // if used without the cordova distribution. Reads CORDOVA_HOME env variable 
-                if(process.env.CORDOVA_HOME) {
-                    directory = process.env.CORDOVA_HOME;
-                } else {
-                    console.error('Could not find Cordova distribution directory');
-                    console.error('Point CORDOVA_HOME to your phonegap distribution path');
-                    process.exit(3);
-                }
-            }
-            console.log(util.format("%s/lib/%s/bin/create ", directory, platform) + options.join(' '));
-            spawn(util.format("%s/lib/%s/bin/create", directory, platform), options, function(code) {
-                if(code != 0) {
-                    console.error("An error occurred while creating project");
-                } else {
-                    console.log(platform+" project successfully created");
-                }
-            });
-        break;
-        case 'build':
-            console.log(util.format("%s/cordova/debug ", directory));
-            spawn(util.format("%s/cordova/debug", directory), [], function(code) {
-                if(code != 0) {
-                    console.error("An error occurred while building project");
-                }
-            });
-        break;
-        case 'emulate':
-            console.log(util.format("%s/cordova/emulate ", directory));
-            spawn(util.format("%s/cordova/emulate", directory), [], function(code) {
-                if(code != 0) {
-                    console.error("An error occurred while emulating project");
-                }
-            });
-        break;
-        default: 
-            usage(5);
-   }
-}
-
+/*
 function processArguments() {
     process.argv.forEach(function(val, index, array) {
         // this is either the cordova distribution/client directory for the create actions
@@ -102,4 +49,51 @@ function processArguments() {
         }
     });
 }
+*/
+module.exports = {
 
+    help: function help (code) {
+        console.error("usage:  cordova create [platform:[directory]:[package_name]:[project_name]]...|[cordova.conf]");
+        console.error("\tcordova build directory directory...|cordova.conf");
+        console.error("\tcordova emulate directory directory...|cordova.conf");
+        process.exit(code);
+    }
+    ,
+    create: function create (platform, package, name) {
+        var args = [].slice.call(arguments)
+        ,   projectPath = path.join(process.cwd(), name)
+
+        if (args.length != 3) {
+            console.error('Invalid number of arguments.')
+            process.exit(1)
+        }
+        
+        exec(util.format("%s/lib/%s/bin/create %s %s %s", dist, platform, projectPath, package, name), function(err, stdout, stderr) {
+            if (err) {
+                console.error('An error occurred while creating project!', err)
+            } 
+            else {
+                console.log( platform + ' project successfully created.')
+            }
+        })
+    }
+    ,
+    build: function build() {
+        console.log(util.format("%s/cordova/debug ", directory));
+        spawn(util.format("%s/cordova/debug", directory), [], function(code) {
+            if(code != 0) {
+                console.error("An error occurred while building project");
+            }
+        });
+    }
+    ,
+    emulate: function emulate() {
+        console.log(util.format("%s/cordova/emulate ", directory));
+        spawn(util.format("%s/cordova/emulate", directory), [], function(code) {
+            if(code != 0) {
+                console.error("An error occurred while emulating project");
+            }
+        });
+   }
+   // end of module defn
+}
--- test/test-cli.js
+++ test/test-cli.js
@@ -1,4 +1,7 @@
 var should = require('should')
+,   exec   = require('child_process').exec
+,   path   = require('path')
+,   fs     = require('fs')
 
 describe('cordova cli', function() {
 
@@ -7,6 +10,21 @@ describe('cordova cli', function() {
             true.should.eql(true)
             done()
         })
+    })
+    
+    describe('cordova create ios com.foobar Baz', function() {
+        it('should create a project called Baz', function(done) {
+            exec('cordova create ios com.foobar Baz', function(err, stderr, stdout) {
+                path.existsSync(path.join(__dirname, '..', 'Baz')).should.eql(true)
+                done()
+            })
+        })
+    })
+
+    after(function() {
+        var bazDir = path.join(__dirname, '..', 'Baz')
+        // fs.rmdirSync(bazDir) failing due to node bug?
+        exec('rm -rf Baz', function(){})
     })
     // end of tests
 })
