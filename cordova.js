27e2cf6a2dc3c343fb56ab37891e42c7188de6cd
Brian LeRoux <brian@westcoastlogic.com>
2012-07-14 21:36:08 -0700

    gratuitous colors

--- cordova.js
+++ cordova.js
@@ -4,11 +4,34 @@ var fs        = require('fs')
 ,   exec      = require('child_process').exec
 ,   platforms = ['ios', 'android']
 ,   dist      = process.env.CORDOVA_HOME != undefined ? process.env.CORDOVA_HOME : path.join(__dirname, 'lib', 'cordova-1.9.0')
+,   colors    = require('colors')
 
 module.exports = {
 
     help: function help () {
-        console.log(fs.readFileSync(path.join(__dirname, 'doc', 'help.txt')).toString('utf8'))
+        var raw = fs.readFileSync(path.join(__dirname, 'doc', 'help.txt')).toString('utf8').split("\n")
+        console.log(raw.map(function(line) {
+            if (line.match('    ')) {
+                var prompt = '    $ '
+                ,   isPromptLine = !!(line.indexOf(prompt) != -1)
+                if (isPromptLine) {
+                    return prompt.green + line.replace(prompt, '')
+                }
+                else {
+                    return line.split(/\./g).map( function(char) { 
+                        if (char === '') {
+                            return '.'.grey
+                        }
+                        else {
+                            return char
+                        }
+                    }).join('')
+                }
+            }
+            else {
+                return line.magenta
+            }
+        }).join("\n"))
     }
     ,
     create: function create (platform, package, name) {
