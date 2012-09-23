08599f54c011e07b6d06c27a141fbdae270226f5
Fil Maj <maj.fil@gmail.com>
2012-09-23 11:17:18 -0700

    axed asyncblock. tweaked test_bootstrap

--- test_bootstrap.js
+++ test_bootstrap.js
@@ -1,17 +1,18 @@
+/**
+ * TEST BOOTSTRAP FILE
+ * Runs through any bs to make sure the tests are good to go.
+ **/
+
 var fs = require('fs'),
     path = require('path'),
     util = require('./src/util'),
-    asyncblock = require('asyncblock'),
     platforms = require('./platforms');
 
-// Simply detects whether all platform libs have been cloned.
-
-asyncblock(function(flow) {
-    platforms.forEach(function(p) {
-        if (!fs.existsSync(path.join(__dirname, 'lib', p))) {
-            util.getPlatformLib(p, flow);
-        }
-    });
-    process.exit(0);
+// If a platform library dependency does not exist, will clone it down.
+platforms.forEach(function(p) {
+    if (!fs.existsSync(path.join(__dirname, 'lib', p))) {
+        util.getPlatformLib(p);
+    }
 });
+process.exit(0);
 
