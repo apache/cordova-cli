31ac868d9e8856697b6d1f5f635ad5b5ae653310
Fil Maj <maj.fil@gmail.com>
2012-09-30 23:34:25 -0700

    added blackberry shortcut for project creation to speed up specs

--- spec/helper.js
+++ spec/helper.js
@@ -3,7 +3,8 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 var shell = require('shelljs'),
     path = require('path'),
     fs = require('fs'),
-    android_project = path.join(__dirname, 'fixtures', 'projects', 'native', 'android');
+    android_project = path.join(__dirname, 'fixtures', 'projects', 'native', 'android'),
+    bb_project = path.join(__dirname, 'fixtures', 'projects', 'native', 'blackberry');
 
 var orig_exec = shell.exec;
 
@@ -18,6 +19,14 @@ module.exports = {
                 var dir = r.exec(cmd)[1];
                 shell.cp('-r', android_project, path.join(dir, '..'));
                 fs.chmodSync(path.join(dir, 'cordova', 'debug'), '754');
+                return {code:0};
+            } else if (cmd.match(/blackberry.bin.create/)) {
+                var r = new RegExp(/blackberry.bin.create"\s"([\/\\\w-_\.]*)"/);
+                var dir = r.exec(cmd)[1];
+                var platformsDir = path.join(dir, '..');
+                shell.cp('-r', bb_project, platformsDir);
+                // TODO: will need to handle bb sub-platforms
+                shell.mv(path.join(platformsDir, 'blackberry'), path.join(platformsDir, 'blackberry-10'));
                 return {code:0};
             }
             // Fire off to original exec
