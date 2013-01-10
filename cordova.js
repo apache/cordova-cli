e71881f0fc133765635c48d4981c27cc4ee71a93
Fil Maj <maj.fil@gmail.com>
2013-01-10 13:37:06 -0800

    route "platforms" command to platform, same with "plugins" -> plugin

--- cordova.js
+++ cordova.js
@@ -4,9 +4,11 @@ module.exports = {
     help:     require('./src/help'),
     create:   require('./src/create'),
     platform: require('./src/platform'),
+    platforms: require('./src/platform'),
     build:    require('./src/build'),
     emulate:  require('./src/emulate'),
     plugin:   require('./src/plugin'),
+    plugins:   require('./src/plugin'),
     serve:    require('./src/serve'),
     on:       function() {
         cordova_events.on.apply(cordova_events, arguments);
