f6dc3ad12a44f96348c900fc53205044c09e3eab
Fil Maj <maj.fil@gmail.com>
2013-02-06 10:37:17 -0800

    updating to cordova-android 2.4.0

--- lib/cordova-android/framework/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/Users/fil/src/cordova-cli/lib/cordova-android/framework/gen/org/apache/cordova/R.java \
- : /Users/fil/src/cordova-cli/lib/cordova-android/framework/res/drawable/splash.png \
-/Users/fil/src/cordova-cli/lib/cordova-android/framework/res/drawable-hdpi/icon.png \
-/Users/fil/src/cordova-cli/lib/cordova-android/framework/res/drawable-ldpi/icon.png \
-/Users/fil/src/cordova-cli/lib/cordova-android/framework/res/drawable-mdpi/icon.png \
-/Users/fil/src/cordova-cli/lib/cordova-android/framework/res/layout/main.xml \
-/Users/fil/src/cordova-cli/lib/cordova-android/framework/res/values/strings.xml \
-/Users/fil/src/cordova-cli/lib/cordova-android/framework/res/xml/config.xml \
-/Users/fil/src/cordova-cli/lib/cordova-android/framework/bin/AndroidManifest.xml \
