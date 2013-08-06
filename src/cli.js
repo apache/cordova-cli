3b1724da822d18376bd09150c6b3aadc7d9dbc76
Lucas Holmquist <lholmqui@redhat.com>
2013-08-06 17:04:58 -0400

    CB-2234 Add 'cordova info' command

--- src/cli.js
+++ src/cli.js
@@ -85,6 +85,10 @@ module.exports = function CLI(inputArgs) {
         return cordova.help();
     }
 
+    if (cmd === "info") {
+        return cordova.info();
+    }
+
     if (cordova.hasOwnProperty(cmd)) {
         if (cmd == 'emulate' || cmd == 'build' || cmd == 'prepare' || cmd == 'compile' || cmd == 'run') {
             // Filter all non-platforms into options
