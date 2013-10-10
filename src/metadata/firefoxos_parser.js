4260a88ed458bf83de6dd676568fa6024334d3c3
Steven Gill <stevengill97@gmail.com>
2013-10-10 16:40:02 -0700

    CB-4957: added fix for FFOS

--- src/metadata/firefoxos_parser.js
+++ src/metadata/firefoxos_parser.js
@@ -115,10 +115,10 @@ module.exports.prototype = {
         this.update_www();
 
         return this.update_from_config(cfg)
-        .then(function() {
+        .then(function(){
             this.update_overrides();
             this.update_staging();
             util.deleteSvnFolders(this.www_dir());
-        });
+        }.bind(this));
     }
 };
