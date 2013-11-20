c2d031b8646332d51739c53ca81005742599fa69
Steven Gill <stevengill97@gmail.com>
2013-11-19 16:42:52 -0800

    updated to 3.2.0-0.1.0

--- platforms.js
+++ platforms.js
@@ -21,41 +21,41 @@ module.exports = {
     'ios' : {
         parser : './src/metadata/ios_parser',
         url    : 'https://git-wip-us.apache.org/repos/asf?p=cordova-ios.git',
-        version: '3.2.0-rc1'
+        version: '3.2.0'
     }, 
     'android' : {
         parser : './src/metadata/android_parser',
         url    : 'https://git-wip-us.apache.org/repos/asf?p=cordova-android.git',
-        version: '3.2.0-rc1'
+        version: '3.2.0'
     }, 
     'wp7' : {
         parser : './src/metadata/wp7_parser',
         url    : 'https://git-wip-us.apache.org/repos/asf?p=cordova-wp8.git',
-        version: '3.2.0-rc1'
+        version: '3.2.0'
     },
     'wp8' : {
         parser : './src/metadata/wp8_parser',
         url    : 'https://git-wip-us.apache.org/repos/asf?p=cordova-wp8.git',
-        version: '3.2.0-rc1'
+        version: '3.2.0'
     },
     'blackberry10' : {
         parser : './src/metadata/blackberry10_parser',
         url    : 'https://git-wip-us.apache.org/repos/asf?p=cordova-blackberry.git',
-        version: '3.2.0-rc1'
+        version: '3.2.0'
     },
     'www':{
         url    : 'https://git-wip-us.apache.org/repos/asf?p=cordova-app-hello-world.git',
-        version: '3.2.0-rc1'
+        version: '3.2.0'
     },
     'firefoxos':{
         parser: './src/metadata/firefoxos_parser',
         url    : 'https://git-wip-us.apache.org/repos/asf?p=cordova-firefoxos.git',
-        version: '3.2.0-rc1'
+        version: '3.2.0'
     },
     'windows8':{
         parser: './src/metadata/windows8_parser',
         url    : 'https://git-wip-us.apache.org/repos/asf?p=cordova-windows.git',
-        version: '3.2.0-rc1'
+        version: '3.2.0'
     }
 };
 
