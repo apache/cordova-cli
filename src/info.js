31ea565bac97a6f35c484cba3461fb726f24e07a
Martin Gonzalez <martin.c.glez.glez@gmail.com>
2014-04-02 12:09:13 -0600

    CB-6329 added Q logic to work with callbacks
    -Added, proper indent (4 spaces).
    -Removed, trailing whitespace
    -Fixed, callbacks double assignation.
    - \r replaced by \n.
    
    Also, now it uses Q to work with callbacks, this time is going to return
    every value retrieve from the environment, all promises it will be
    called, those promises are callbacks data, Q.allSettled is going to call
    all of them, and then wait until every single one of them is fulfilled
    or rejected, then is going to print the data and save into a file.
    Adding this logic, it returns promises, and cleans the code of heavy
    nesting of callbacks.

--- src/info.js
+++ src/info.js
@@ -16,97 +16,98 @@
     specific language governing permissions and limitations
     under the License.
 */
-var cordova_util  = require('./util'),
-path          = require('path'),
-fs            = require('fs'),
-Q             = require('q'),
-info_utils 	  = require('./info-utils');
+    var cordova_util  = require('./util'),
+        path          = require('path'),
+        fs            = require('fs'),
+        child_process = require('child_process'),
+        Q             = require('q'),
+        info_utils 	  = require('./info-utils');
 
-/*
-    A utility funciton to help output the information needed
-    when submitting a help request.
+    /*
+        A utility funciton to help output the information needed
+        when submitting a help request.
 
-    Outputs to a file
-*/
+        Outputs to a file
+    */
 module.exports = function info() {
-    //Get projectRoot 
-    var projectRoot = cordova_util.cdProjectRoot(),
-    output;
-    delLog(projectRoot);
-    //Get Node info
-    info_utils.getNodeInfo(function (result){
-        if( result ){
-            print_SaveMsg(projectRoot, "Node version: "+ result);
-            //Get Cordova version
-            info_utils.getCordovaInfo(function (result){
-                if( result ){print_SaveMsg(projectRoot,"Cordova version: "+ result );
-                    // Get config.xml file
-                    fs.readFile(cordova_util.projectConfig(projectRoot) , 'utf-8' , function ( err , result ){
-                        if(err) {print_SaveMsg(projectRoot,"Error reading config.xml file"+ err);}
-                        print_SaveMsg(projectRoot,'Config.xml File: \r'+ result +'\r\r\r');
-                        print_SaveMsg(projectRoot,'Plugins: \r' + doPlugins( projectRoot ) +'\r\r\r');
-                        //Get platforms info
-                        doPlatforms(projectRoot, function (result){
-                            if( result ){
-                                print_SaveMsg(projectRoot,result);
-                            }
-                        });
-                    });
-                }
-            });
+        //Get projectRoot 
+        var projectRoot = cordova_util.cdProjectRoot(),
+        output;
+        if (!projectRoot) {
+            return Q.reject( new Error('Current working directory is not a Cordova-based project.') );
         }
-    });
-    return Q();
+
+        delLog(projectRoot);
+        //Array of functions, Q.allSettled
+        return Q.allSettled([ (function (){
+            console.log("Collecting Data...");
+            //Get Node version
+            return (Q.denodeify (info_utils.getNodeInfo)());
+        }()), (function (){
+            //Get Cordova version
+            return (Q.denodeify (info_utils.getCordovaInfo)());
+        }()), (function (){
+           //Get project config.xml file
+           return 'Config.xml File: \n\n'+ (fs.readFileSync(cordova_util.projectConfig(projectRoot), 'utf-8')) +'\n\n\n'
+        }()), (function (){
+             //Get list of plugins
+            return 'Plugins: \n\n' + doPlugins( projectRoot ) +'\n\n\n';
+        }()), (function (){
+            //Get Platforms information
+            return (Q.denodeify (doPlatforms)(projectRoot));
+        }())]).then(function (promises){
+            for(var t in promises)
+            print_SaveMsg( projectRoot, (function (){ return promises[t].state==='fulfilled' ? promises[t].value : promises[t].state==='rejected' ? promises[t].reason : 'Still working' }()) );
+       });
 };
 
-function delLog(projectRoot){
-    if(fs.existsSync(path.join(projectRoot,'info.txt'))){
-        fs.unlink(path.join(projectRoot,'info.txt'), function (err) {
-            if (err) {throw err; console.log("Unable to delete info.txt file \r");}
-            writeLog(projectRoot, '');
-        });
-    }else{writeLog(projectRoot, '');}
-}
-function writeLog (projectRoot, data){
-    //Successfully deleted, writing new one
-    fs.writeFile(path.join(projectRoot,'info.txt'), data, 'utf-8', function (err) {
-        if (err) throw err;
-    });
-}
-function print_SaveMsg(projectRoot, data){
-    console.log(data);
-    appendLog(projectRoot,data)
-    
-}
-function appendLog(projectRoot, data){
-
-    fs.appendFile(path.join(projectRoot,'info.txt'), data, 'utf-8', function (err) {
-        if (err) throw err;
-    });
-
-}
-
-function doPlatforms( projectRoot, result){
-    var platforms = cordova_util.listPlatforms(projectRoot);
-    if( platforms.length ) {
-        for(var i=0; i<platforms.length; i++){
-            info_utils.getPlatformInfo( platforms[ i ], projectRoot, function(callback){
-                result(callback);
+    function delLog(projectRoot){
+        if(fs.existsSync(path.join(projectRoot,'info.txt'))){
+            fs.unlink(path.join(projectRoot,'info.txt'), function (err) {
+                if (err) {throw err; console.log("Unable to delete info.txt file \n");}
+                writeLog(projectRoot, '');
             });
+        }else{writeLog(projectRoot, '');}
+    }
+    function writeLog (projectRoot, data){
+        //Successfully deleted, writing new one
+        fs.writeFile(path.join(projectRoot,'info.txt'), data, 'utf-8', function (err) {
+            if (err) throw err;
+        });
+    }
+    function print_SaveMsg(projectRoot, data){
+        console.info(data);
+        appendLog(projectRoot,data)
+    }
+    function appendLog(projectRoot, data){
+
+        fs.appendFile(path.join(projectRoot,'info.txt'), data, 'utf-8', function (err) {
+            if (err) throw err;
+        });
+
+    }
+
+    function doPlatforms( projectRoot, result){
+        var platforms = cordova_util.listPlatforms(projectRoot);
+        if( platforms.length ) {
+            for(var i=0; i<platforms.length; i++){
+                info_utils.getPlatformInfo( platforms[ i ], projectRoot, function(callback){
+                    result(callback);
+                });
+            }
+        }
+        else {
+            result("No Platforms Currently Installed");
         }
     }
-    else {
-        result("No Platforms Currently Installed");
-    }
-}
 
-function doPlugins( projectRoot ){
-    var pluginPath = path.join(projectRoot, 'plugins'),
-    plugins = cordova_util.findPlugins(pluginPath);
+    function doPlugins( projectRoot ){
+        var pluginPath = path.join(projectRoot, 'plugins'),
+        plugins = cordova_util.findPlugins(pluginPath);
 
-    if( !plugins.length ) {
-        return "No Plugins Currently Installed";
-    } else {
-        return plugins;
+        if( !plugins.length ) {
+            return "No Plugins Currently Installed";
+        } else {
+            return plugins;
+        }
     }
-}
