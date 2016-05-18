5af9bd1c5b09b26bf610c2d65112842b1da73305
Omar Mefire <omefire@gmail.com>
2016-05-18 13:50:50 -0700

    Introducing utils module

--- src/cli.js
+++ src/cli.js
@@ -26,11 +26,8 @@
 var path = require('path'),
     fs = require('fs'),
     help = require('./help'),
-    nopt,
-    _,
-    updateNotifier,
-    pkg = require('../package.json'),
     telemetry = require('./telemetry'),
+    utils = require('./utils'),
     Q = require('q');
 
 var cordova_lib = require('cordova-lib'),
@@ -38,48 +35,6 @@ var cordova_lib = require('cordova-lib'),
     cordova = cordova_lib.cordova,
     events = cordova_lib.events,
     logger = require('cordova-common').CordovaLogger.get();
-
-
-/*
- * init
- *
- * initializes nopt and underscore
- * nopt and underscore are require()d in try-catch below to print a nice error
- * message if one of them is not installed.
- */
-function init() {
-    try {
-        nopt = require('nopt');
-        _ = require('underscore');
-        updateNotifier = require('update-notifier');
-    } catch (e) {
-        console.error(
-            'Please run npm install from this directory:\n\t' +
-            path.dirname(__dirname)
-        );
-        process.exit(2);
-    }
-}
-
-function checkForUpdates() {
-    try {
-        // Checks for available update and returns an instance
-        var notifier = updateNotifier({
-            pkg: pkg
-        });
-
-        // Notify using the built-in convenience method
-        notifier.notify();
-    } catch (e) {
-        // https://issues.apache.org/jira/browse/CB-10062
-        if (e && e.message && /EACCES/.test(e.message)) {
-            console.log('Update notifier was not able to access the config file.\n' +
-                'You may grant permissions to the file: \'sudo chmod 744 ~/.config/configstore/update-notifier-cordova.json\'');
-        } else {
-            throw e;
-        }
-    }
-}
 
 // For CordovaError print only the message without stack trace unless we
 // are in a verbose mode.
@@ -94,18 +49,18 @@ process.on('uncaughtException', function (err) {
     
     
 var shouldCollectTelemetry = false;
-module.exports = function (inputArgs, cb) {
+function main(inputArgs, cb) {
     
     /**
      * mainly used for testing.
      */
     cb = cb || function(){};
     
-    init();
+    utils.init();
     
-    checkForUpdates();
+    utils.checkForUpdates();
     
-    var args = parseArguments(inputArgs || process.argv);
+    var args = utils.parseArguments(inputArgs || process.argv);
         
     logger.subscribe(events); 
 
@@ -179,106 +134,6 @@ module.exports = function (inputArgs, cb) {
     }).done();
 };
 
-function parseArguments(inputArgs) {
-    // When changing command line arguments, update doc/help.txt accordingly.
-    var knownOpts =
-        {
-            'verbose': Boolean
-            , 'version': Boolean
-            , 'help': Boolean
-            , 'silent': Boolean
-            , 'experimental': Boolean
-            , 'noregistry': Boolean
-            , 'nohooks': Array
-            , 'shrinkwrap': Boolean
-            , 'copy-from': String
-            , 'link-to': path
-            , 'searchpath': String
-            , 'variable': Array
-            , 'link': Boolean
-            , 'force': Boolean
-            // Flags to be passed to `cordova build/run/emulate`
-            , 'debug': Boolean
-            , 'release': Boolean
-            , 'archs': String
-            , 'device': Boolean
-            , 'emulator': Boolean
-            , 'target': String
-            , 'browserify': Boolean
-            , 'noprepare': Boolean
-            , 'fetch': Boolean
-            , 'nobuild': Boolean
-            , 'list': Boolean
-            , 'buildConfig': String
-            , 'template': String
-        };
-
-    var shortHands =
-        {
-            'd': '--verbose'
-            , 'v': '--version'
-            , 'h': '--help'
-            , 'src': '--copy-from'
-            , 't': '--template'
-        };
-
-    var args = nopt(knownOpts, shortHands, inputArgs); 
-    
-    // If there were arguments protected from nopt with a double dash, keep
-    // them in unparsedArgs. For example:
-    // cordova build ios -- --verbose --whatever
-    // In this case "--verbose" is not parsed by nopt and args.vergbose will be
-    // false, the unparsed args after -- are kept in unparsedArgs and can be
-    // passed downstream to some scripts invoked by Cordova.
-    var unparsedArgs = [];
-    var parseStopperIdx =  args.argv.original.indexOf('--');
-    if (parseStopperIdx != -1) {
-        unparsedArgs = args.argv.original.slice(parseStopperIdx + 1);
-    }
-
-    // args.argv.remain contains both the undashed args (like platform names)
-    // and whatever unparsed args that were protected by " -- ".
-    // "undashed" stores only the undashed args without those after " -- " .
-    var remain = args.argv.remain;
-    var undashed = remain.slice(0, remain.length - unparsedArgs.length);
-    
-    args.command = undashed[0];
-    
-    // ToDO: Test cordova, cordova --help, cordova --h, cordova bogus
-    if ( !args.command || args.command == 'help' || args.help ) {
-        if(!args.help && remain[0] === 'help') {
-            remain.shift();
-        } 
-        args.command = 'help';
-    } else if (args.command === 'platform' || args.command === 'platforms' || args.command === 'plugin' || args.command === 'plugins') {
-        args.subcommand = undashed[1];
-        args.targets = undashed.slice(2); // array of targets, either platforms or plugins
-        var cli_vars = {};
-        if (args.variable) {
-            args.variable.forEach(function (s) {
-                // CB-9171
-                var eq = s.indexOf('=');
-                if (eq == -1)
-                    throw new CordovaError("invalid variable format: " + s);
-                var key = s.substr(0, eq).toUpperCase();
-                var val = s.substr(eq + 1, s.length);
-                cli_vars[key] = val;
-            });
-        }
-        args.cli_vars = cli_vars;
-    } else if(args.command === 'telemetry') {
-        args.subcommand = undashed[1];
-    } else if(args.command === 'serve') {
-        args.port = args.undashed[1];
-    }
-    
-    args.undashed = undashed;
-    args.remain = remain;
-    args.unparsedArgs = unparsedArgs; 
-    
-    return args;
-}
-
 function cli(args) {
 
     if (args.version) {
@@ -337,7 +192,7 @@ function cli(args) {
 
         return cordova.raw[args.command].call(null, opts);
     } else if (args.command === 'telemetry') {
-        return handleTelemetryCmd(args.subcommand, telemetry.isOptedIn());
+        return handleTelemetryCmd(args.subcommand);
     } else if (args.command === 'requirements') {
         // All options without dashes are assumed to be platform names
         opts.platforms = undashed.slice(1);
@@ -450,13 +305,13 @@ function cli(args) {
     
     // ToDO: Move telemetry tracking upwards
     // ToDO: Check if command succeeded or failed upwards
-    function handleTelemetryCmd(args) {
+    function handleTelemetryCmd(subcommand) {
         
-        if (args.subcommand !== 'on' && args.subcommand !== 'off') {
+        if (subcommand !== 'on' && subcommand !== 'off') {
             return help(['telemetry']);
         }
         
-        var turnOn = args.subcommand === 'on' ? true : false;
+        var turnOn = subcommand === 'on' ? true : false;
 
         // turn telemetry on or off
         if (turnOn) {
@@ -470,3 +325,5 @@ function cli(args) {
         return Q();
     }
 }
+
+module.exports = main;
