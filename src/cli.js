d014c9fe8feaca423978385ce177fdac9deb8a11
Audrey So <audreyso@apache.org>
2017-03-09 14:54:03 -0800

    CB-12008 : updated .txt docs to reflect new autosave changes and removed variables missed due to rebase

--- src/cli.js
+++ src/cli.js
@@ -218,46 +218,6 @@ function handleTelemetryCmd(subcommand, isOptedIn) {
 }
 
 function cli(inputArgs) {
-    // When changing command line arguments, update doc/help.txt accordingly.
-    var knownOpts =
-        { 'verbose' : Boolean
-        , 'version' : Boolean
-        , 'help' : Boolean
-        , 'silent' : Boolean
-        , 'experimental' : Boolean
-        , 'noregistry' : Boolean
-        , 'nohooks': Array
-        , 'shrinkwrap' : Boolean
-        , 'copy-from' : String
-        , 'link-to' : path
-        , 'searchpath' : String
-        , 'variable' : Array
-        , 'link': Boolean
-        , 'force': Boolean
-        // Flags to be passed to `cordova build/run/emulate`
-        , 'debug' : Boolean
-        , 'release' : Boolean
-        , 'archs' : String
-        , 'device' : Boolean
-        , 'emulator': Boolean
-        , 'target' : String
-        , 'browserify': Boolean
-        , 'noprepare': Boolean
-        , 'fetch': Boolean
-        , 'nobuild': Boolean
-        , 'list': Boolean
-        , 'buildConfig' : String
-        , 'template' : String
-        , 'nosave' : Boolean
-        };
-
-    var shortHands =
-        { 'd' : '--verbose'
-        , 'v' : '--version'
-        , 'h' : '--help'
-        , 'src' : '--copy-from'
-        , 't' : '--template'
-        };
 
     checkForUpdates();
 
@@ -434,7 +394,7 @@ function cli(inputArgs) {
         } else {
             args.save = true;
         }
-        
+
         var download_opts = { searchpath : args.searchpath
                             , noregistry : args.noregistry
                             , nohooks : args.nohooks
