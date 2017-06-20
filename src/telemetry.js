417504e2d005ace6902ca6bdeb5507fa665c7f7b
Jesse MacFadyen <purplecabbage@gmail.com>
2017-06-19 22:19:06 -0700

    added timeoutInSecs property to make user prompts testable without waiting for 30 seconds

--- src/telemetry.js
+++ src/telemetry.js
@@ -39,35 +39,36 @@ var insight = new Insight({
     pkg: pkg
 });
 
+
 /**
  * Returns true if the user opted in, and false otherwise
  */
 function showPrompt() {
 
     var deferred = Q.defer();
-    
+
     var msg = 'May Cordova anonymously report usage statistics to improve the tool over time?';
+    insight._permissionTimeout = module.exports.timeoutInSecs || 30;
     insight.askPermission(msg, function (unused, optIn) {
         var EOL = require('os').EOL;
         if (optIn) {
             console.log(EOL + 'Thanks for opting into telemetry to help us improve cordova.');
-            track('telemetry', 'on', 'via-cli-prompt-choice', 'successful');
+            module.exports.track('telemetry', 'on', 'via-cli-prompt-choice', 'successful');
         } else {
             console.log(EOL + 'You have been opted out of telemetry. To change this, run: cordova telemetry on.');
             // Always track telemetry opt-outs! (whether opted-in or opted-out)
-            track('telemetry', 'off', 'via-cli-prompt-choice', 'successful');
+            module.exports.track('telemetry', 'off', 'via-cli-prompt-choice', 'successful');
         }
-        
-        deferred.resolve(optIn); 
+        deferred.resolve(optIn);
     });
-    
+
     return deferred.promise;
 }
 
 function track() {
     // Remove empty, null or undefined strings from arguments
     for (var property in arguments) {
-        var val = arguments[property]; 
+        var val = arguments[property];
         if (!val || val.length === 0) {
             delete arguments.property;
         }
@@ -118,6 +119,7 @@ function isNoTelemetryFlag(args) {
     return args.indexOf('--no-telemetry') > -1;
 }
 
+// this is to help testing, so we don't have to wait for the full 30
 module.exports = {
     track: track,
     turnOn: turnOn,
@@ -127,5 +129,6 @@ module.exports = {
     hasUserOptedInOrOut: hasUserOptedInOrOut,
     isCI: isCI,
     showPrompt: showPrompt,
-    isNoTelemetryFlag: isNoTelemetryFlag
+    isNoTelemetryFlag: isNoTelemetryFlag,
+    timeoutInSecs:30
 };
